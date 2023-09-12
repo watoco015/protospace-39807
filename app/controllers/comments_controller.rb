aac07f0b73f7a922b5c380f272eaecd429e107f4
watoco015 <fuwari015@gmail.com>
2023-09-12 15:02:48 +0900

    コメント投稿可能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
+      @comment = Comment.create(comment_params)
       @prototype = Prototype.find(params[:prototype_id])
-      @comment = Comment.new
       @comments = @prototype.comments.includes(:user)
-      
       if @comment.save
-        redirect_to "/prototypes/#{@comment.prototype.id}" # Corrected path and variable
+        redirect_to "/prototypes/#{@comment.prototype.id}"
       else
         render "prototypes/show"
       end
   end
-
 
 private
   def comment_params
